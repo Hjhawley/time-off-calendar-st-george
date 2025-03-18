31f67b0724c37b1ab081a0660417dee759120a28
Hayden <hjhawley7@gmail.com>
2025-03-18 13:19:19 -0600

    fixed gitignore

--- config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "YOUR_FIREBASE_API_KEY",
-    authDomain: "timeoffcalendar-1d3ab.firebaseapp.com",
-    projectId: "timeoffcalendar-1d3ab",
-    storageBucket: "timeoffcalendar-1d3ab.appspot.com",
-    messagingSenderId: "959934939505",
-    appId: "1:959934939505:web:2883b8fed3506adba076d0"
-};
